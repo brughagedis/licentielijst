7d5f4f64bead0af9ae6b8aa324511192143c6aa7
brughagedis <44606611+brughagedis@users.noreply.github.com>
2018-12-12 12:23:22 +0100

    Update error.php

--- licentielijstmap/licentielijsten/error.php
+++ licentielijstmap/licentielijsten/error.php
@@ -42,16 +42,7 @@
       <script type="text/javascript" src="/licentielijstmap/Editor/js/dataTables.editor.min.js"></script>
       <script src="/licentielijstmap/DataTables/Bootstrap-3.3.7/js/bootstrap.min.js"></script>        
       <script src="js/menuscript.js"></script>   
-      <script>
-         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-         
-         ga('create', 'UA-20266439-2', 'auto');
-         ga('send', 'pageview');
-         
-      </script>        
+            
    </head>
    <body>
       <table id="wrapper" border="0" cellspacing="0">
