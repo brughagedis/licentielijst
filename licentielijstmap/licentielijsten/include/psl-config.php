c2c1b981a8b2216a11c4c58bdc0959ff90b261b2
brughagedis <44606611+brughagedis@users.noreply.github.com>
2018-12-12 12:20:46 +0100

    Update psl-config.php

--- licentielijstmap/licentielijsten/include/psl-config.php
+++ licentielijstmap/licentielijsten/include/psl-config.php
@@ -3,9 +3,9 @@
  * These are the database login details
  */  
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "sec_user");    // The database username. 
-define("PASSWORD", "yUuycdLJh4bvLa6h");    // The database password. 
-define("DATABASE", "readersonline_all");    // The database name.
+define("USER", "");    // The database username. 
+define("PASSWORD", "");    // The database password. 
+define("DATABASE", "readersonline");    // The database name.
  
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
