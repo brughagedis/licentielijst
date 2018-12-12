026f4ed96a287a508e3b3604448fee7ea07845ff
brughagedis <44606611+brughagedis@users.noreply.github.com>
2018-12-12 12:25:51 +0100

    Update login.php

--- licentielijstmap/licentielijsten/login.php
+++ licentielijstmap/licentielijsten/login.php
@@ -50,16 +50,7 @@
       <script src="js/menuscript.js"></script> 
       <script type="text/JavaScript" src="js/sha512.js"></script> 
       <script type="text/JavaScript" src="js/forms.js"></script> 
-      <script>
-         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-         
-         ga('create', 'UA-20266439-2', 'auto');
-         ga('send', 'pageview');
-         
-      </script>       
+       
    </head>
    <body>
       <table id="wrapper" border="0" cellspacing="0">
